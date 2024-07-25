d79950d6e24f3e24979f61932f896a0e3183d09a
clumsyCoder00 <nuthanael.watkins@gmail.com>
2024-07-25 09:28:17 -0400

    Update soundGAZ.py
    Initial upload of my revised script.

--- soundGAZ.py
+++ soundGAZ.py
@@ -1,24 +1,32 @@
 # Imports
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+import mutagen
+
 import os
 import time
+import datetime
 
 # user-defined URL of profile
-
 # EXAMPLE: "https://soundgasm.net/u/USERNAME"
-url = "<- Paste https://soundgasm.net/u/ user profile link here ->"
+
+url = "https://soundgasm.net/u/<USERNAME>"
 
 # username extraction for folder creation
 userSplit = url.split("/")
 user = userSplit[-1]
 
-try:
-    make_Command = "mkdir {0}".format(user)
-    os.system(make_Command)
-except Exception as e:
-    print(e)
-    pass
+totalItems = 0
+newItems = 0
+existItems = 0
+
+userDirectory = "/mnt/pond/media/Audio/GWA/{0}".format(user)
+
+if not(os.path.exists(userDirectory)):
+  make_Command = "mkdir " + userDirectory
+  os.system(make_Command)
 
 #  driver options boilerplate #
 
@@ -32,91 +40,79 @@ options.add_argument("user-data-dir=selenium")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])  # automation detection evasion
 options.add_experimental_option('useAutomationExtension', False)  # automation detection evasion
 
-driver = webdriver.Chrome(options=options, executable_path="./sel_driver/chromedriver")
+# driver = webdriver.Chrome(options=options, executable_path="./sel_driver/chromedriver")
+
+driver = webdriver.Chrome(options=options)
 driver.set_page_load_timeout(60)
-print("Loading...")
+
+recording_driver = webdriver.Chrome(options=options)
+recording_driver.set_page_load_timeout(60)
+
+print("Getting file list...")
 
 ####################################################################################
 
-# variable set up
-name = "unspecified"
-title = "unspecified"
-link = "unspecified"
-try:
-    # profile get
-    driver.get(url)
-    # media posts extraction
-    for sound_link in driver.find_elements_by_xpath('/html/body/div[*]/a'):
-        sound_link.get_attribute("href")
-        print(sound_link.get_attribute("href"))
-        print(sound_link.get_attribute("href"), file=open("raw.txt", "a"))
-except Exception as e:
-    print(e)
-    pass
+# profile get
+driver.get(url)
+# media posts extraction
+fileList = driver.find_elements(By.XPATH,'/html/body/div[*]/a')
+totalItems = len(fileList)
 
-##############################################################################
-url = ""
-
-f = open("raw.txt")
-links = f.read().splitlines()
-
-# content download Loop
-for link_line in links:
-    if link_line != "":
-
-        print(link_line)
-        url = link_line
+print("Checking files...")
+for sound_link in fileList:	
         # variable Flushing
-        name = ""
-        title = ""
-        link = ""
+	name = ""
+	title = ""
+	link = ""
+	descr = ""
 
-        # main driver get for media page
-        try:
-            driver.get(url)
-            time.sleep(2)  # slow your roll let it load
-        except Exception as e:
-            print(e)
-            pass
-        # account name extraction
-        try:
-            name = driver.find_element_by_xpath('/html/body/div[1]/a', ).text.replace("'", "").replace('"', "").replace(
-                '/', " ").replace('*', " ")
-            print(name)
-        except Exception as e:
-            print(e)
-            pass
-        # Title creation
-        # Using this method we leverage the websites title character stripping instead of doing it ourselves.
-        try:
-            url_split = url.split("/")
-            title = url_split[-1].replace("-", " ")  # make pretty
-            print(title)
-        except Exception as e:
-            print(e)
-            pass
-        # extracting media link
-        try:
-            link = driver.find_element_by_xpath('//*[@id="jp_audio_0"]').get_attribute("src")
-            print(link)
-        except Exception as e:
-            print(e)
-            pass
-        # command generation and execution
-        try:
-            w_Get_command = "wget {0} -c -O './{1}/{2} {3}.m4a'".format(link, user, name, title)
-            print(w_Get_command)
-            os.system(w_Get_command)
-        except Exception as e:
-            print(e)
-            pass
+	recordingPage = sound_link.get_attribute("href")
+	url_split = recordingPage.split("/")
 
-    else:
-        pass
+	# ---- get recording title ----
+	title = url_split[-1].replace("-", " ")  # make pretty
 
+	recording_path = '/mnt/pond/media/Audio/GWA/{0}/{1}.m4a'.format(user, title)
+	if not(os.path.isfile(recording_path)):
+		#print("Title: " + title)
+		recording_driver.get(recordingPage)
+		time.sleep(2)  # slow your roll let it load, req'd to give time for file url to return
+	
+		# ---- get artist ----
+		# name = recording_driver.find_element(By.XPATH,'/html/body/div[1]/a', ).text.replace("'", "").replace('"', "").replace('/', " ").replace('*', " ")
+		# print("name: " + name)
 
-# clean up
+		# ---- get recording description ----
+		descr = recording_driver.find_element(By.CLASS_NAME,"jp-description").text
+		descr = descr.replace("'", "") # removing quote characters from metadata, see TIDAL script for more correct fix
+		# print("descr: " + descr)
 
+		# ---- get audio file url ----
+		link = recording_driver.find_element(By.XPATH,'//*[@id="jp_audio_0"]').get_attribute("src")
+		# print("link: " + link)
+	
+		# ---- download audio file ----
+		w_Get_command = "wget {0} -nv -c -O '{1}' > /dev/null 2>&1".format(link, recording_path)# .split()
+		os.system(w_Get_command)
+    
+		# ---- get date of soundgasm file, seems to be the upload date ----            
+		modDate = os.path.getmtime(recording_path) # %m/%d/%Y
+		releaseDate = (datetime.datetime.fromtimestamp(modDate).strftime("%m-%d-%Y"))
+		print(releaseDate + " " + title)
+		with open(recording_path, 'r+b') as file:
+			media_file = mutagen.File(file, easy=True)
+			media_file['title'] = title
+			media_file['album'] = title
+			media_file['artist'] = user
+			media_file['comment'] = descr
+			media_file['date'] = releaseDate
+			media_file.save(file)
+		newItems = newItems + 1
+
+	else:
+		existItems = existItems + 1
+		# print("\talready exists")
+
+print("\n---- Track Summary ----\nExisting:\t" + str(existItems) + "\nNew:\t\t" + str(newItems)+ "\nTotal:\t\t" + str(totalItems))
 driver.quit()
-# zeroing out file
-open("raw.txt", 'w').close()
+recording_driver.quit()
